6b440c306b818d7f8c3e2a0f115c9591c2bb06ef
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2022-09-04 14:42:28 +0300

    feat: fetching characters data from API to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,55 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import requests from "../services/requests";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    characterList: [],
+    characterData: null,
+  },
+
   getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+
+  mutations: {
+    SET_CHARACTER_LIST(state, characters) {
+      state.characterList.push(...characters);
+    },
+
+    RESET_CHARACTER_LIST(state) {
+      state.characterList = [];
+    },
+
+    SET_CHARACTER_DATA(state, character) {
+      state.characterData = character;
+    },
+  },
+
+  actions: {
+    async fetchCharactersList({ commit }, payload) {
+      commit("SET_LOADING", true);
+
+      return requests
+        .fetchCharactersList(payload)
+        .then((characters) => {
+          commit("SET_CHARACTER_LIST", characters);
+          commit("SET_LOADING", false);
+        })
+        .catch((error) => console.log(error));
+    },
+
+    async fetchCharacter({ commit }, payload) {
+      commit("SET_LOADING", true);
+
+      return requests
+        .fetchCharacter(payload)
+        .then((character) => {
+          commit("RESET_CHARACTER_LIST"); // remove characterList
+          commit("SET_CHARACTER_DATA", character);
+          commit("SET_LOADING", false);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
 });
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,50 @@
 <template>
   <div class="center grid custom-grid">
-    <vs-row> </vs-row>
+    <vs-row>
+      <HeroCard
+        v-for="character in characters"
+        :key="character.id"
+        :name="character.name"
+        :thumbnail="character.thumbnail"
+        :availableSeries="character.series?.available || 0"
+        :returnedSeries="character.series?.returned || 0"
+        :availableStories="character.stories?.available || 0"
+        :returnedStories="character.stories?.returned || 0"
+      />
+    </vs-row>
   </div>
 </template>
 
 <script>
+import HeroCard from "../components/HeroCard.vue";
 export default {
   name: "HomePage",
+  components: { HeroCard },
+
+  data() {
+    return {
+      pageLimitSize: 30,
+    };
+  },
+
+  methods: {
+    getFirstList() {
+      if (!this.characters.length || this.characters.length === 0)
+        this.$store.dispatch("fetchCharactersList", {
+          limit: this.pageLimitSize,
+        });
+    },
+  },
+
+  mounted() {
+    this.getFirstList();
+  },
+
+  computed: {
+    characters() {
+      return this.$store.state.characterList || null;
+    },
+  },
 };
 </script>
 
