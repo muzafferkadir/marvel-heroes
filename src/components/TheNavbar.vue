8e53968449c986b679c243ff0d07fbe0bdcbce3c
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2022-09-04 14:30:19 +0300

    feat: navbar component

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-light bg-light justify-content-between">
+    <div class="container">
+      <router-link to="/" class="navbar-brand">
+        <img src="../assets/marvel.png" alt="" height="60px" loading="lazy"
+      /></router-link>
+      <div class="d-flex"></div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.navbar.bg-light {
+  background: #e62429 !important;
+}
+
+@media screen and (max-width: 900px) {
+  .navbar {
+    padding-bottom: 16px;
+  }
+
+  .navbar .container > * {
+    display: block;
+    width: 100%;
+  }
+
+  .navbar-brand {
+    text-align: center;
+    margin-bottom: 8px;
+  }
+}
+</style>
