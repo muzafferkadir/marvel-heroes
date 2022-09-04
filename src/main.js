794be6bdd88e103b53ba3c332e2bfe19329c3436
muzafferkadir <muzafferkadiryilmaz@gmail.com>
2022-09-04 14:45:20 +0300

    fix: vuesax css bugs

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import router from "./router";
 import store from "./store";
 import APIRequests from "./services/requests";
 
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
 Vue.config.productionTip = false;
 
 Vue.use(Vuesax, {});
